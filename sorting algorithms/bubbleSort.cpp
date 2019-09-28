694b65f9808b35515584c5bf950dd98e6d1cb7de
Shivam Mishra <41479737+codder-shivam@users.noreply.github.com>
2019-09-28 11:14:30 +0530

    Update bubbleSort.cpp

--- sorting algorithms/bubbleSort.cpp
+++ sorting algorithms/bubbleSort.cpp
@@ -1,41 +1,45 @@
 #include<iostream>
 using namespace std;
 
-void bubbleSort(int arr[] , int);
+void bubbleSort(int arr[] , int); // driver finction to implement bubble sort algorithm
 
 int main()
 {
-cout<<"\n Enter number of input elements: ";
-int size;
-cin>>size;
-cout<<"\n Enter elements of array: ";
-int arr[size];
-for(int i=0;i<size;i++)cin>>arr[i];
+  cout<<"\n Enter number of input elements: ";
+  int size;
+  cin>>size;
+  cout<<"\n Enter elements of array: ";
+  int arr[size];
+  for(int i=0;i<size;i++)
+    cin>>arr[i];
+  
+  bubbleSort(arr,size);   //function called
 
-bubbleSort(arr,size);
+  for(int i=0;i<size;i++)
+    cout<<arr[i]<<" ";
+  cout<<endl;
 
-for(int i=0;i<size;i++)
-cout<<arr[i]<<" ";
-cout<<endl;
-
-return 0;
+  return 0;
 }
+
+ //modified algorithm for bubble sort which avoids unnecssary comparisaon of sorted elements 
 
 void bubbleSort(int arr[] , int n)
 {
-int i=1,flag=1,j,temp;
-while(i<n && flag){
-flag = 0;
+  int i=1,flag=1,j,temp;
+  while(i<n && flag){
+    flag = 0;
 
-for(j=0;j< n-i;j++){
-if(arr[j]>arr[j+1])
-{
-flag=1;
-temp=arr[j+1];
-arr[j+1]=arr[j];
-arr[j]=temp;
-}
-}
-i++;
-}
+    for(j=0;j< n-i;j++)
+    {
+      if(arr[j]>arr[j+1])
+      {
+        flag=1;
+        temp=arr[j+1];
+        arr[j+1]=arr[j];
+        arr[j]=temp;
+      }
+    }
+    i++;
+  }
 }
